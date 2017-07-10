0bd8b0b47398999e18b9cea50741b7c7bd2828e5
naniwaKun <radio.naniwa@gmail.com>
2017-07-11 01:59:29 +0900

    Update init.rb

--- init.rb
+++ init.rb
@@ -2,8 +2,8 @@ require "twitter"
 
 $name = "fustuu_no_ladio"
 $twh  = ""
-$c_k   = "5732szMAGpQYZB4Cwn4Rt9f4Y"
-$c_s   = "VXERJ48qBcmRbqbBrdk25fIsHlznO0eIF5z9kVoCenEYkVhO6A"
-$a_t   = "708231555437604864-hMAckqRJtAABg5XYFsxIF8egso0hcsh"
-$a_t_s = "7ljpsmGLLSDbh9nxFS42gDZD6WplAWKLCApz1XwCeGpgx"
+$c_k   = ""
+$c_s   = ""
+$a_t   = ""
+$a_t_s = ""
 
